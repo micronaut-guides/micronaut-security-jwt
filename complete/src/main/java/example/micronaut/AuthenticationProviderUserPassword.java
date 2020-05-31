699245f2253648706818ca120fc540ce92d77913
Sergio del Amo <sergio.delamo@softamo.com>
2020-05-31 07:52:21 +0200

    fix: use onError for AuthenticationFailed

--- complete/src/main/java/example/micronaut/AuthenticationProviderUserPassword.java
+++ complete/src/main/java/example/micronaut/AuthenticationProviderUserPassword.java
@@ -2,6 +2,7 @@ package example.micronaut;
 
 import edu.umd.cs.findbugs.annotations.Nullable;
 import io.micronaut.http.HttpRequest;
+import io.micronaut.security.authentication.AuthenticationException;
 import io.micronaut.security.authentication.AuthenticationFailed;
 import io.micronaut.security.authentication.AuthenticationProvider;
 import io.micronaut.security.authentication.AuthenticationRequest;
@@ -24,7 +25,7 @@ public class AuthenticationProviderUserPassword implements AuthenticationProvide
                     authenticationRequest.getSecret().equals("password")) {
                 emitter.onNext(new UserDetails((String) authenticationRequest.getIdentity(), new ArrayList<>()));
             } else {
-                emitter.onNext(new AuthenticationFailed());
+                emitter.onError(new AuthenticationException(new AuthenticationFailed()));
             }
             emitter.onComplete();
         }, BackpressureStrategy.ERROR);
